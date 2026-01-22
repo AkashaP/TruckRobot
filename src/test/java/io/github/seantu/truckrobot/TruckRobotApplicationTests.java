962c67b97c2eb26ffe23f69c37c31bac188153e3
Sean Tu <sean.rico.tu@gmail.com>
2026-01-22 15:58:28 +1000

    Define a TDD suite for testing (domain logic only)

--- src/test/java/io/github/seantu/truckrobot/TruckRobotApplicationTests.java
+++ src/test/java/io/github/seantu/truckrobot/TruckRobotApplicationTests.java
@@ -1,18 +1,141 @@
 package io.github.seantu.truckrobot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+//import org.springframework.boot.test.context.SpringBootTest;
+//@SpringBootTest
 class TruckRobotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    /*@Test
+    void contextLoads() {
+    }*/
+    // Initial TDD tests
 
     @Test
-    void truckPlaced() {
-
+    void truckIsNotPlacedUntilPlacedIsCalled() {
+        TruckRobot robot = new TruckRobot();
+        assertFalse(robot.isPlaced());
+        robot.place(0,0, Facing.SOUTH);
+        assertTrue(robot.isPlaced());
+        assertEquals("0,0,SOUTH", robot.report());
     }
 
+    @Test
+    void truckReportIsConsistent() {
+        TruckRobot robot = new TruckRobot();
+        robot.place(0,0, Facing.SOUTH);
+        assertEquals("0,0,SOUTH", robot.report());
+        robot.place(1,1, Facing.WEST);
+        assertEquals("1,1,WEST", robot.report());
+        robot.place(4,4, Facing.EAST);
+        assertEquals("4,4,EAST", robot.report());
+        robot.place(3,3, Facing.NORTH);
+        assertEquals("3,3,NORTH", robot.report());
+    }
+
+    @Test
+    void truckIgnoresTurnsWhenNotPlaced() {
+        TruckRobot robot = new TruckRobot();
+        robot.turn(Direction.LEFT);
+        robot.turn(Direction.RIGHT);
+        assertEquals("ROBOT MISSING", robot.report());
+    }
+
+    @Test
+    void truckMovesForward() {
+        TruckRobot robot = new TruckRobot();
+        robot.place(0,0, Facing.NORTH);
+        robot.forward();
+        assertEquals("0,1,NORTH", robot.report());
+        robot.forward();
+        assertEquals("0,2,NORTH", robot.report());
+    }
+
+    @Test
+    void truckTurns() {
+        TruckRobot robot = new TruckRobot();
+        robot.place(0,0, Facing.NORTH);
+        robot.turn(Direction.RIGHT);
+        assertEquals("0,0,EAST", robot.report());
+        robot.turn(Direction.RIGHT);
+        assertEquals("0,0,SOUTH", robot.report());
+        robot.turn(Direction.RIGHT);
+        assertEquals("0,0,WEST", robot.report());
+        robot.turn(Direction.LEFT);
+        assertEquals("0,0,SOUTH", robot.report());
+        robot.turn(Direction.LEFT);
+        assertEquals("0,0,EAST", robot.report());
+        robot.turn(Direction.LEFT);
+        assertEquals("0,0,NORTH", robot.report());
+    }
+
+    @Test
+    void truckMovesInSnakePath() {
+        TruckRobot robot = new TruckRobot();
+        robot.place(0,2, Facing.NORTH);
+
+        // Move right by one
+        robot.turn(Direction.RIGHT);
+        assertEquals("0,2,RIGHT", robot.report());
+        robot.forward();
+        assertEquals("1,2,RIGHT", robot.report());
+
+        // Go to the bottom
+        robot.turn(Direction.RIGHT);
+        assertEquals("1,2,SOUTH", robot.report());
+        robot.forward();
+        assertEquals("1,1,SOUTH", robot.report());
+
+        // Move right by one
+        robot.turn(Direction.LEFT);
+        assertEquals("1,1,WEST", robot.report());
+        robot.forward();
+        assertEquals("2,1,WEST", robot.report());
+
+        // Go back up
+        robot.turn(Direction.LEFT);
+        assertEquals("2,1,NORTH", robot.report());
+        robot.forward();
+        assertEquals("2,2,NORTH", robot.report());
+        robot.forward();
+        assertEquals("2,3,NORTH", robot.report());
+    }
+
+    @Test
+    void truckIgnoresOutOfBoundsPlacement() {
+        TruckRobot robot = new TruckRobot();
+        robot.place(0,0, Facing.SOUTH);
+        robot.place(-1,-1, Facing.SOUTH);
+        assertEquals("0,0,SOUTH", robot.report());
+        robot.place(5,0, Facing.NORTH);
+        assertEquals("0,0,SOUTH", robot.report());
+        robot.place(5,5, Facing.EAST);
+        assertEquals("0,0,SOUTH", robot.report());
+    }
+
+    @Test
+    void truckIgnoresMoveWhenNotPlaced() {
+        TruckRobot robot = new TruckRobot();
+        robot.forward();
+        assertEquals("ROBOT MISSING", robot.report());
+    }
+
+    @Test
+    void truckDoesntMoveForwardIfAtEdge() {
+        TruckRobot robot = new TruckRobot();
+        robot.place(0,0, Facing.SOUTH);
+        robot.forward();
+        assertEquals("0,0,SOUTH", robot.report());
+
+        robot.place(0,4, Facing.NORTH);
+        robot.forward();
+        assertEquals("0,4,NORTH", robot.report());
+
+        robot.place(4,4, Facing.EAST);
+        robot.forward();
+        assertEquals("4,4,EAST", robot.report());
+
+        robot.place(0,0, Facing.WEST);
+        robot.forward();
+        assertEquals("0,0,WEST", robot.report());
+    }
 }
