15ab85840a55ac4bbd1a550b8e89a0fa71def86b
Sean Tu <sean.rico.tu@gmail.com>
2026-01-22 14:26:43 +1000

    Initial commit - Spring Initializr web

---
+++ src/test/java/io/github/seantu/truckrobot/TruckrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seantu.truckrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckrobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
